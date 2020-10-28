60f0877471b53b9a6944659c77b418035c95d577
ItzSjDude <Support@itzsjdude.in>
2020-10-28 14:46:09 +0000

    pylint: auto fixes

--- plugins/_inlinebot.py
+++ plugins/_inlinebot.py
@@ -103,10 +103,10 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         else:
             reply_pop_up_alert = "Please get your own PikaBot, and don't use mine!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-    
+
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
     async def on_plug_in_callback_query_handler(event):
-        usr=event.query
+        usr = event.query
         if (
             usr.user_id == b1.id
             or usr.user_id == b2.id
@@ -116,9 +116,10 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             await event.edit("Pika Pi! Menu Closed!")
 
         else:
-            reply_pop_up_alert = "You can't close this menu ploxx, Get your own Pikachu Userbot"
+            reply_pop_up_alert = (
+                "You can't close this menu ploxx, Get your own Pikachu Userbot"
+            )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-
 
     @tgbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
