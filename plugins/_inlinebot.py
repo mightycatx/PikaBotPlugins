f0b9239f9acf4bc4ba3ede3b1abe087e36279476
Sachin Jangir <support@ItzSjDude.in>
2020-10-29 06:22:04 +0530

    Update _inlinebot.py

--- plugins/_inlinebot.py
+++ plugins/_inlinebot.py
@@ -182,7 +182,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
     helpable_plugins = []
     pfix = ["_", "herok", "tool"]
     for p in loaded_plugins:
-        if not p.startswith(tuple(pfix):
+        if not p.startswith(tuple(pfix)):
             helpable_plugins.append(p)
 
     helpable_plugins = sorted(helpable_plugins)
