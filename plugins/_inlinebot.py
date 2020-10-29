9b218f68e27617117f32dd090e7406aefcbc129c
Sachin Jangir <support@ItzSjDude.in>
2020-10-29 11:06:44 +0530

    Update _inlinebot.py

--- plugins/_inlinebot.py
+++ plugins/_inlinebot.py
@@ -123,9 +123,9 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             or usr.user_id == b3.id
             or usr.user_id == b4.id
         ):
-            a = await event.edit("Pika Pi! Restarting wait for 1 Min!")
+            await event.edit("Pika Pi! Restarting wait for 1 Min!")
             pika_start()
-            await a.delete()
+            await event.delete()
         else:
             reply_pop_up_alert = "You can't Restart me, Get your own Pikachu Userbot"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
