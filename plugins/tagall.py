b4280bf54723c88137a6a8ef9860a3354aed2f69
Sachin Jangir <support@ItzSjDude.in>
2020-10-30 18:50:46 +0530

    Update tagall.py

--- plugins/tagall.py
+++ plugins/tagall.py
@@ -4,13 +4,13 @@
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="tagall"))
+@ItzSjDude(pattern="tagall")
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "Idhar aao saalo🚶"
+    mentions = "Attention Please"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, 100):
-        mentions += f"[\u2063](tg://user?id={x.id})"
+        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
     await event.reply(mentions)
     await event.delete()
