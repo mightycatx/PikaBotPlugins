da1625a5893354d45be881c25cb3220ab1a1ce97
ItzSjDude <Support@itzsjdude.in>
2020-11-01 23:57:25 +0000

    pylint: auto fixes

--- plugins/_inlinebot.py
+++ plugins/_inlinebot.py
@@ -206,7 +206,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = pikrws
     number_of_cols = pikcl
     helpable_plugins = []
-    #pfix = ["_", "tool"]
+    # pfix = ["_", "tool"]
     for p in loaded_plugins:
         if not p.startswith("_"):
             helpable_plugins.append(p)
