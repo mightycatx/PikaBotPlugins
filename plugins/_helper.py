1e1c58a2b9899376216c517b05bbd5ae1e0c115d
Sachin Jangir <support@ItzSjDude.in>
2020-11-06 15:35:56 +0530

    Update _helper.py

--- plugins/_helper.py
+++ plugins/_helper.py
@@ -11,34 +11,25 @@ async def cmd_list(event):
         if tgbotusername is None or input_str == "text":
             string = ""
             for i in CMD_LIST:
-                string += "\n" + i + "\n"
+                string += "ℹ️ " + i + "\n"
                 for iter_list in CMD_LIST[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
                 string += "\n"
             if len(string) > 4095:
-                with io.BytesIO(str.encode(string)) as out_file:
-                    out_file.name = "cmd.txt"
-                    await event.client.send_file(
-                        event.chat_id,
-                        out_file,
-                        force_document=True,
-                        allow_cache=False,
-                        caption="**COMMANDS**",
-                        reply_to=reply_to_id,
-                    )
-                    await event.delete()
+                await borg.send_message(event.chat_id, "Do .help cmd")
+                await asyncio.sleep(5)
             else:
                 await event.edit(string)
         elif input_str:
             if input_str in CMD_LIST:
-                string = "Commands found in {}:".format(input_str)
+                string = "Commands found in {}:\n".format(input_str)
                 for i in CMD_LIST[input_str]:
                     string += "    " + i
                     string += "\n"
                 await event.edit(string)
             else:
-                await event.edit(input_str + " is not a valid plugin!")
+                await event.edit(input_str + " is not a Plugin Nibba!😕")
         else:
             help_string = f"""Pïkå¢hµ Úsêrßð† {helpstr}"""
             results = await event.client.inline_query(  # pylint:disable=E0602
