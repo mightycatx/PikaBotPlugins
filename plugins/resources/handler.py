2b1c5874eba79cfca10a2a36945ee93474aa7e13
Sachin Jangir <support@ItzSjDude.in>
2020-11-10 18:39:58 +0530

    Optimizations for PikaBot 2.1 🚶

--- plugins/resources/handler.py
+++ plugins/resources/handler.py
@@ -10,6 +10,22 @@ try:
 except BaseException:
     pass
 
+if bot is not None:
+   pika_id1 = i1
+else:
+   pika_id1 = 0110
+if bot2 is not None:
+   pika_id2 = i2
+else:
+   pika_id2 = 0101
+if bot3 is not None:
+   pika_id3 = i3
+else:
+   pika_id3 = 1010
+if bot4 is not None:
+   pika_id4 = i4
+else:
+   pika_id4 = 1001
 
 def pikaa(a, shortname):
     try:
