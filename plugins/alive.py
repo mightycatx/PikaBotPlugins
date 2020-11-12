b1e1c4d78c22d24d9e79c4c5107e98ce1cdb59c4
Sachin Jangir <support@ItzSjDude.in>
2020-11-12 14:17:07 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -3,8 +3,8 @@
 """
 # Made by @ItzSjDude. All Rights Reserved
 
-from .plugins import alive
+from . import _alive
 
 @ItzSjDude(outgoing=True, pattern=r"alive$")
 async def _(event):
-    await alive(event)
+    await _alive(event)
