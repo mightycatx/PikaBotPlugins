58cbe07ade2d9e1499619961eee6d31c1919f21b
ItzSjDude <Support@itzsjdude.in>
2020-11-12 07:34:15 +0000

    PikaBot Fixes🚶

--- plugins/dns.py
+++ plugins/dns.py
@@ -4,19 +4,19 @@ Available Commands:
 {i}dns google.com
 {i}url <long url>
 {i}unshort <short url>"""
-#Credits @UniBorg 
+# Credits @UniBorg
+
 
 @ItzSjDude(outgoing=True, pattern="dns (.*)")
 async def _(event):
-     await dns(event)
+    await dns(event)
+
 
 @ItzSjDude(outgoing=True, pattern="url (.*)")
 async def _(event):
     await urlx(event)
 
 
-
 @ItzSjDude(outgoing=True, pattern="unshort (.*)")
 async def _(event):
     await unshort(event)
-
