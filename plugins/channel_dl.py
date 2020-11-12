768cf7c853cf70db269822d9ddb132bb7a6ea904
Sachin Jangir <support@ItzSjDude.in>
2020-11-12 11:44:00 +0530

    Update channel_dl.py

--- plugins/channel_dl.py
+++ plugins/channel_dl.py
@@ -4,66 +4,12 @@ usage: {i}geta channel_username [will  get all media from channel, tho there is 
        {i}getc number_of_messsages channel_username
 """
 # By: @Zero_cool7870
-import os
-import subprocess
-
-from pikabot.utils import ItzSjDude
-
 
 @ItzSjDude(outgoing=True, pattern=r"getc")
-async def get_media(event):
-    if event.fwd_from:
-        return
-    dir = "./temp/"
-    try:
-        os.makedirs("./temp/")
-    except BaseException:
-        pass
-    channel_username = event.text
-    limit = channel_username[6:9]
-    print(limit)
-    channel_username = channel_username[11:]
-    print(channel_username)
-    await event.edit("Downloading Media From this Channel.")
-    msgs = await event.client.get_messages(channel_username, limit=int(limit))
-    with open("log.txt", "w") as f:
-        f.write(str(msgs))
-    for msg in msgs:
-        if msg.media is not None:
-            await event.client.download_media(msg, dir)
-    ps = subprocess.Popen(("ls", "temp"), stdout=subprocess.PIPE)
-    output = subprocess.check_output(("wc", "-l"), stdin=ps.stdout)
-    ps.wait()
-    output = str(output)
-    output = output.replace("b'", "")
-    output = output.replace("\n'", "")
-    await event.edit("Downloaded " + output + " files.")
-
+async def _(event):
+  await get_media(event)
 
 @ItzSjDude(pattern=r"geta")
-async def get_media(event):
-    if event.fwd_from:
-        return
-    dir = "./temp/"
-    try:
-        os.makedirs("./temp/")
-    except BaseException:
-        pass
-    channel_username = event.text
-    channel_username = channel_username[7:]
+async def _(event):
+  await getmedia(event)
 
-    print(channel_username)
-    await event.edit("Downloading All Media From this Channel.")
-    msgs = await event.client.get_messages(channel_username, limit=3000)
-    with open("log.txt", "w") as f:
-        f.write(str(msgs))
-    for msg in msgs:
-        if msg.media is not None:
-            await event.client.download_media(msg, dir)
-    ps = subprocess.Popen(("ls", "temp"), stdout=subprocess.PIPE)
-    output = subprocess.check_output(("wc", "-l"), stdin=ps.stdout)
-    ps.wait()
-    output = str(output)
-    output = output.replace("b'", "")
-    output = output.replace("\n'", "")
-    await event.edit("Downloaded " + output + " files.")
