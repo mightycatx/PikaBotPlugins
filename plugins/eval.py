ba8d0e9578c8bdbeee326e366993ebb146b1707f
Sachin Jangir <support@ItzSjDude.in>
2020-11-12 16:55:17 +0530

    Update eval.py

--- plugins/eval.py
+++ plugins/eval.py
@@ -1,7 +1,8 @@
 """Evaluate Python Code inside Telegram
 {i}eval PythonCode"""
 
+from . import _eval 
 
 @ItzSjDude(outgoing=True, pattern="eval")
 async def _(event):
-    await eval(event)
+    await _eval(event)
