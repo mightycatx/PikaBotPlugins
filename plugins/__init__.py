4a81da8e593d60099c6b0bf604792bc9d80120b3
Sachin Jangir <support@ItzSjDude.in>
2020-11-12 14:48:02 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -866,7 +866,7 @@ async def muter(moot):
     """ Used for deleting the messages of muted people """
     try:
         from pikabot.sql_helper.gmute_sql import is_gmuted
-        from pikabot.sql_helper.spam_mute_sql import is_muted
+        from pikabot.sql_helper.mute_sql import is_muted
     except AttributeError:
         return
     muted = is_muted(moot.chat_id)
