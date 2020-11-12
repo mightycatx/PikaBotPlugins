d6b1d41e01f303f3bfe589b797f1a19147c7a27a
Sachin Jangir <support@ItzSjDude.in>
2020-11-12 12:04:24 +0530

    Update decide.py

--- plugins/decide.py
+++ plugins/decide.py
@@ -1,18 +1,7 @@
 """Quickly make a decision
-Syntax: .decide"""
-import requests
-from pikabot.utils import ItzSjDude
-
+{i}decide"""
 
 @ItzSjDude(outgoing=True, pattern="decide")
 async def _(event):
-    if event.fwd_from:
-        return
-    message_id = event.message.id
-    if event.reply_to_msg_id:
-        message_id = event.reply_to_msg_id
-    r = requests.get("https://yesno.wtf/api").json()
-    await event.client.send_message(
-        event.chat_id, r["answer"], reply_to=message_id, file=r["image"]
-    )
-    await event.delete()
+  await decide(event)
+
