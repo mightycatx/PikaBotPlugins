10bd2d24ae2b75e889d0af2db24c7383e7322b9d
Sachin Jangir <support@ItzSjDude.in>
2020-11-13 07:08:47 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3624,3 +3624,117 @@ async def _fwd(event):
         await event.client.forward_messages(event.chat_id, fwd_message)
         await fwd_message.delete()
         await event.delete()
+
+async def download_file_from_google_drive(id):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params={"id": id}, stream=True)
+    token = await get_confirm_token(response)
+    if token:
+        params = {"id": id, "confirm": token}
+        response = session.get(URL, params=params, stream=True)
+
+    headers = response.headers
+    content = headers["Content-Disposition"]
+    destination = await get_file_name(content)
+
+    file_name = await save_response_content(response, destination)
+    return file_name
+
+
+async def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith("download_warning"):
+            return value
+
+    return None
+
+
+async def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+    return destination
+
+
+async def gdrive_file_id(link):  # Extract File Id from G-Drive Link
+    file_id = ""
+    c_append = False
+    if link[1:33] == "https://drive.google.com/file/d/":
+        link = link[33:]
+        fid = ""
+        for c in link:
+            if c == "/":
+                break
+            fid = fid + c
+        return fid
+    for c in link:
+        if c == "=":
+            c_append = True
+        if c == "&":
+            break
+        if c_append:
+            file_id = file_id + c
+    file_id = file_id[1:]
+    return file_id
+
+
+async def get_file_name(content):
+    file_name = ""
+    c_append = False
+    for c in str(content):
+        if c == '"':
+            c_append = True
+        if c == ";":
+            c_append = False
+        if c_append:
+            file_name = file_name + c
+    file_name = file_name.replace('"', "")
+    print("File Name: " + str(file_name))
+    return file_name
+
+async def _gdl(event):
+    if event.fwd_from:
+        return
+    drive_link = event.text[4:]
+    print("Drive Link: " + drive_link)
+    file_id = await gdrive_file_id(drive_link)
+    await event.edit("Downloading Requested File from G-Drive...")
+    file_name = await download_file_from_google_drive(gdrive_file_id)
+    await event.edit("File Downloaded.\nName: `" + str(get_file_name) + "`")
+
+async def _gbot(event):
+    if event.fwd_from:
+        return
+    mentions = "**🤖Bots in this Chat**: \n"
+    input_str = event.pattern_match.group(1)
+    to_write_chat = await event.get_input_chat()
+    chat = None
+    if not input_str:
+        chat = to_write_chat
+    else:
+        mentions = "🤖Bots in {} : \n".format(input_str)
+        try:
+            chat = await event.client.get_entity(input_str)
+        except Exception as e:
+            await event.edit(str(e))
+            return None
+    try:
+        async for x in event.client.iter_participants(
+            chat, filter=ChannelParticipantsBots
+        ):
+            if isinstance(x.participant, ChannelParticipantAdmin):
+                mentions += "\n 🔥 [{}](tg://user?id={}) `{}`".format(
+                    x.first_name, x.id, x.id
+                )
+            else:
+                mentions += "\n [{}](tg://user?id={}) `{}`".format(
+                    x.first_name, x.id, x.id
+                )
+    except Exception as e:
+        mentions += " " + str(e) + "\n"
+    await event.edit(mentions)
