61875e295b5115db8802980e347e15a07efc187d
Sachin Jangir <support@ItzSjDude.in>
2020-11-13 07:14:02 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -34,6 +34,11 @@ from requests import get
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from telethon import custom, events
+from telethon.tl.types import (
+    ChannelParticipantAdmin,
+    ChannelParticipantCreator,
+    ChannelParticipantsAdmins,
+)
 from telethon.errors import (
     BadRequestError,
     ChatAdminRequiredError,
@@ -3738,3 +3743,59 @@ async def _gbot(event):
     except Exception as e:
         mentions += " " + str(e) + "\n"
     await event.edit(mentions)
+
+async def _gadmins(event):
+    if event.fwd_from:
+        return
+    mentions = "**Admins in this Channel**: \n"
+    should_mention_admins = False
+    reply_message = None
+    pattern_match_str = event.pattern_match.group(1)
+    if "m" in pattern_match_str:
+        should_mention_admins = True
+        if event.reply_to_msg_id:
+            reply_message = await event.get_reply_message()
+    input_str = event.pattern_match.group(2)
+    to_write_chat = await event.get_input_chat()
+    chat = None
+    if not input_str:
+        chat = to_write_chat
+    else:
+        mentions_heading = "Admins in {} channel: \n".format(input_str)
+        mentions = mentions_heading
+        try:
+            chat = await event.client.get_entity(input_str)
+        except Exception as e:
+            await event.edit(str(e))
+            return None
+    try:
+        async for x in event.client.iter_participants(
+            chat, filter=ChannelParticipantsAdmins
+        ):
+            if not x.deleted:
+                if isinstance(x.participant, ChannelParticipantCreator):
+                    mentions += "\n 🔱 [{}](tg://user?id={}) `{}`".format(
+                        x.first_name, x.id, x.id
+                    )
+        mentions += "\n"
+        async for x in event.client.iter_participants(
+            chat, filter=ChannelParticipantsAdmins
+        ):
+            if not x.deleted:
+                if isinstance(x.participant, ChannelParticipantAdmin):
+                    mentions += "\n 🥇 [{}](tg://user?id={}) `{}`".format(
+                        x.first_name, x.id, x.id
+                    )
+            else:
+                mentions += "\n `{}`".format(x.id)
+    except Exception as e:
+        mentions += " " + str(e) + "\n"
+    if should_mention_admins:
+        if reply_message:
+            await reply_message.reply(mentions)
+        else:
+            await event.reply(mentions)
+        await event.delete()
+    else:
+        await event.edit(mentions)
+
