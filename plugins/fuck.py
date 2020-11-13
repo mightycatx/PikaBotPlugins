7ca7e1979529b82ce6e01adef884b5a18c4527e1
Sachin Jangir <support@ItzSjDude.in>
2020-11-13 06:50:04 +0530

    Update fuck.py

--- plugins/fuck.py
+++ plugins/fuck.py
@@ -1,95 +1,11 @@
+"""18+ friendly animations 
+{i}fuck
+{i}kess
+{i}sux
 """
+from . import _fuck
 
-Available Commands:
-
-.sux
-
-.fuk
-
-.kiss"""
-
-
-import asyncio
-
-from pikabot.utils import ItzSjDude
-
-
-@ItzSjDude(outgoing=True, pattern="fuck")
+@ItzSjDude(outgoing=True, pattern="?(.)")
 async def _(event):
+     await _fuck(event)
 
-    if event.fwd_from:
-
-        return
-
-    animation_interval = 1
-
-    animation_ttl = range(0, 101)
-
-    # input_str = event.pattern_match.group(1)
-
-    # if input_str == "fuk":
-
-    await event.edit("fuk")
-
-    animation_chars = ["👉       ✊️", "👉     ✊️", "👉  ✊️", "👉✊️💦"]
-
-    for i in animation_ttl:
-
-        await asyncio.sleep(animation_interval)
-
-        await event.edit(animation_chars[i % 4])
-
-
-@ItzSjDude(outgoing=True, pattern="sux")
-async def _(event):
-
-    if event.fwd_from:
-
-        return
-
-    animation_interval = 1
-
-    animation_ttl = range(0, 101)
-
-    # input_str = event.pattern_match.group(1)
-
-    # if input_str == "sux":
-
-    await event.edit("sux")
-
-    animation_chars = ["🤵       👰", "🤵     👰", "🤵  👰", "🤵👼👰"]
-
-    for i in animation_ttl:
-
-        await asyncio.sleep(animation_interval)
-
-        await event.edit(animation_chars[i % 4])
-
-
-""
-
-
-@ItzSjDude(outgoing=True, pattern="kiss")
-async def _(event):
-
-    if event.fwd_from:
-
-        return
-
-    animation_interval = 1
-
-    animation_ttl = range(0, 101)
-
-    # input_str = event.pattern_match.group(1)
-
-    # if input_str == "kiss":
-
-    await event.edit("kiss")
-
-    animation_chars = ["🤵       👰", "🤵     👰", "🤵  👰", "🤵💋👰"]
-
-    for i in animation_ttl:
-
-        await asyncio.sleep(animation_interval)
-
-        await event.edit(animation_chars[i % 4])
