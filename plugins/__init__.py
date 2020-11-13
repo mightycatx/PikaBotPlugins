573b92c4fd22aec3009e5fffdd36797350b0c6a0
Sachin Jangir <support@ItzSjDude.in>
2020-11-13 17:02:03 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3603,21 +3603,32 @@ async def _fuck(event):
     animation_ttl = range(0, 101)
     input_str = event.pattern_match.group(1)
     if input_str == "fuck":
+
         await event.edit("fuck")
         animation_chars = ["👉       ✊️", "👉     ✊️", "👉  ✊️", "👉✊️💦"]
+
         for i in animation_ttl:
+
             await asyncio.sleep(animation_interval)
             await event.edit(animation_chars[i % 4])
+
     if input_str == "kiss":
+
         await event.edit("kiss")
         animation_chars = ["🤵       👰", "🤵     👰", "🤵  👰", "🤵💋👰"]
+
         for i in animation_ttl:
+
             await asyncio.sleep(animation_interval)
             await event.edit(animation_chars[i % 4])
+
     if input_str == "sux":
+
         await event.edit("sux")
         animation_chars = ["🤵       👰", "🤵     👰", "🤵  👰", "🤵👼👰"]
+
         for i in animation_ttl:
+
             await asyncio.sleep(animation_interval)
             await event.edit(animation_chars[i % 4]
  
