29cf7ca4e162a51fe2554f7afd6423d058a5fd18
Sachin Jangir <support@ItzSjDude.in>
2020-11-13 05:49:07 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3429,3 +3429,34 @@ def paginate_help(page_number, loaded_plugins, prefix):
         )
 
     return pairs
+
+
+async def _currency(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    input_str = event.pattern_match.group(1)
+    input_sgra = input_str.split(" ")
+    if len(input_sgra) == 3:
+        try:
+            number = float(input_sgra[0])
+            currency_from = input_sgra[1].upper()
+            currency_to = input_sgra[2].upper()
+            request_url = "https://api.exchangeratesapi.io/latest?base={}".format(
+                currency_from
+            )
+            current_response = requests.get(request_url).json()
+            if currency_to in current_response["rates"]:
+                current_rate = float(current_response["rates"][currency_to])
+                rebmun = round(number * current_rate, 2)
+                await event.edit(
+                    "{} {} = {} {}".format(number, currency_from, rebmun, currency_to)
+                )
+            else:
+                await event.edit("IDEKNOWTDWTT")
+        except e:
+            await event.edit(str(e))
+    else:
+        await event.edit("`.currency number from to`")
+    end = datetime.now()
+    (end - start).seconds
