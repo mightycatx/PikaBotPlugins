259785e7efbd1a4a2420a2b1336473e486ec7904
Sachin Jangir <support@ItzSjDude.in>
2020-11-13 06:41:09 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3567,3 +3567,27 @@ async def _fleave(event):
         await asyncio.sleep(animation_interval)
 
         await event.edit(animation_chars[i % 17])
+
+async def _ftext(event):
+    input_str = event.pattern_match.group(1)
+    if input_str:
+        paytext = input_str
+        pay = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(
+            paytext * 8,
+            paytext * 8,
+            paytext * 2,
+            paytext * 2,
+            paytext * 2,
+            paytext * 6,
+            paytext * 6,
+            paytext * 2,
+            paytext * 2,
+            paytext * 2,
+            paytext * 2,
+            paytext * 2,
+        )
+    else:
+        pay = "╭━━━╮\n┃╭━━╯\n┃╰━━╮\n┃╭━━╯\n┃┃\n╰╯\n"
+    # pay = "{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}\n{}".format(paytext*8, paytext*8, paytext*2, paytext*2, paytext*2, paytext*6, paytext*6, paytext*2, paytext*2, paytext*2, paytext*2, paytext*2)
+    await event.edit(pay)
+
