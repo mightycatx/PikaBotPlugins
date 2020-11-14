a561fb7dabc6e1e53e6cfcfe6417d088f544e9d5
ItzSjDude <Support@itzsjdude.in>
2020-11-14 02:08:28 +0000

    PikaBot Fixes🚶

--- plugins/pmmanager.py
+++ plugins/pmmanager.py
@@ -14,7 +14,6 @@ import pikabot.sql_helper.pmpermit_sql as pmpermit_sql
 from pikabot import *
 from pikabot.handler import *
 from telethon import events, functions
-from telethon.tl.functions.users import GetFullUserRequest
 from var import Var
 
 duser = {}
