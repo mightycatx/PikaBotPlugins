f08e6fba5d7aaae7f2ae8643acbfc10b38623ca7
ItzSjDude <Support@itzsjdude.in>
2020-11-20 14:20:47 +0000

    PikaBot Fixes🚶

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1244,15 +1244,15 @@ async def _muter(moot):
         return
     if pika.id == pika_id1:
         try:
-           muted = is_muted(moot.chat_id)
-           gmuted = is_gmuted(moot.sender_id)
-        except:
+            muted = is_muted(moot.chat_id)
+            gmuted = is_gmuted(moot.sender_id)
+        except BaseException:
             pass
     if pika.id == pika_id2:
         try:
-           muted = is_muted2(moot.chat_id)
-           gmuted = is_gmuted2(moot.sender_id)
-        except:
+            muted = is_muted2(moot.chat_id)
+            gmuted = is_gmuted2(moot.sender_id)
+        except BaseException:
             pass
     rights = ChatBannedRights(
         until_date=None,
