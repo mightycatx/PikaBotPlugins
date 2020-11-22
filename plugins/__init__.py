18486654d64bb727783335e971fb14a14be39fef
ItzSjDude <Support@itzsjdude.in>
2020-11-22 04:46:07 +0000

    PikaBot Fixes🚶

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -956,7 +956,9 @@ async def _ungmute(un_gmute):
             if ugmte.is_group:
                 ugchat = ugmte.id
                 try:
-                    await un_gmute.client(EditBannedRequest(ugchat, user.id, UNBAN_RIGHTS))
+                    await un_gmute.client(
+                        EditBannedRequest(ugchat, user.id, UNBAN_RIGHTS)
+                    )
                     pika1 += 1
                 except BaseException:
                     pika2 += 1
