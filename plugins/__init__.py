6e888cc0eed4b957ad66885785e685592b93d796
ItzSjDude <Support@itzsjdude.in>
2020-11-22 16:08:51 +0000

    PikaBot Fixes🚶

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3438,7 +3438,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         ):
             _pika_ = pika_.data_match.group(1).decode("UTF-8")
             _pika = CMD_LIST[_pika_].__doc__.format(i=rx)
-            _pikaB =[(custom.Button.inline("⫷BacK", data="pikab"))]
+            _pikaB = [(custom.Button.inline("⫷BacK", data="pikab"))]
             await pika_.edit(_pika, buttons=_pikaB)
 
         else:
@@ -3450,8 +3450,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     inm
                 )
                 await pika_.answer(reply_pop_up_alert, cache_time=0, alert=True)
-     
-    
+
     @tgbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
             data=re.compile(b"pikab(.*)")
