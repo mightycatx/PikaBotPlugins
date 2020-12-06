dd26f5bb5dd3bb1094ca2586d796df56a9094cda
ItzSjDude <Support@itzsjdude.in>
2020-12-06 00:54:20 +0000

    PikaBot Fixes🚶

--- plugins/gps.py
+++ plugins/gps.py
@@ -3,6 +3,8 @@
 USAGE: Sends you the given location name"""
 
 from . import _gps
+
+
 @ItzSjDude(outgoing=True, pattern="gps ?(.*)")
 async def _(event):
-     await _gps(event)
+    await _gps(event)
