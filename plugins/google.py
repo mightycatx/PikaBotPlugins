15ffa0011f68f3bf8650dc0a3236e88d6d257740
Sachin Jangir <support@ItzSjDude.in>
2020-12-12 05:27:05 +0530

    Update google.py

--- plugins/google.py
+++ plugins/google.py
@@ -3,43 +3,8 @@ Available Commands:
 .go <query> credits to owner of bot
 """
 
-from re import findall
-
-from pikabot.utils import ItzSjDude
-from search_engine_parser import GoogleSearch
-
-
-def progress(current, total):
-    logger.info(
-        "Downloaded {} of {}\nCompleted {}".format(
-            current, total, (current / total) * 100
-        )
-    )
-
+from . import _gsearch 
 
 @ItzSjDude(outgoing=True, pattern="go (.*)")
-async def gsearch(q_event):
-    """ For .google command, do a Google search. """
-    match = q_event.pattern_match.group(1)
-    page = findall(r"page=\d+", match)
-    try:
-        page = page[0]
-        page = page.replace("page=", "")
-        match = match.replace("page=" + page[0], "")
-    except IndexError:
-        page = 1
-    search_args = (str(match), int(page))
-    gsearch = GoogleSearch()
-    gresults = await gsearch.async_search(*search_args)
-    msg = ""
-    for i in range(len(gresults["links"])):
-        try:
-            title = gresults["titles"][i]
-            link = gresults["links"][i]
-            desc = gresults["descriptions"][i]
-            msg += f"[{title}]({link})\n`{desc}`\n\n"
-        except IndexError:
-            break
-    await q_event.edit(
-        "**Search Query:**\n`" + match + "`\n\n**Results:**\n" + msg, link_preview=False
-    )
+async def _(event):
+    await _gsearch(event)
