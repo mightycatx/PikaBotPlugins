d71e67fdfdd917a20a73dd1230354315a05ca317
Sachin Jangir <support@ItzSjDude.in>
2020-12-12 08:13:31 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -79,7 +79,6 @@ from telethon.tl.types import (
     MessageMediaPhoto,
 )
 from telethon.utils import get_input_location, pack_bot_file_id
-from userbot import ALIVE_NAME, CMD_HELP
 from var import Var
 from var import Var as Config
 
