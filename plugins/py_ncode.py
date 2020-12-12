f77ed1fc55ae749870cf2e18d79d73b8a428900d
Sachin Jangir <support@ItzSjDude.in>
2020-12-12 17:48:00 +0530

    Update py_ncode.py

--- plugins/py_ncode.py
+++ plugins/py_ncode.py
@@ -4,35 +4,11 @@
 """
 # created by @Buddhhu, Rebased by @ItzSjDude. All Rights Reserved
 
-import os
 
-import pygments
-from pygments.formatters import ImageFormatter
-from pygments.lexers import Python3Lexer
-
+from . import _ncode 
 
 @ItzSjDude(pattern=r"ncode (.*)")
-async def coder_print(event):
-    input = event.pattern_match.group(1)
-    a = await event.client.download_media(
-        await event.get_reply_message(), Var.TEMP_DOWNLOAD_DIRECTORY
-    )
-    s = open(a, "r")
-    c = s.read()
-    s.close()
-    pygments.highlight(
-        f"{c}",
-        Python3Lexer(),
-        ImageFormatter(font_name="DejaVu Sans Mono", line_numbers=True),
-        "out.png",
-    )
-    if "doc" in input:
-        await event.client.send_file(event.chat_id, "out.png", force_document=True)
-        await event.delete()
-        os.remove(a)
-        os.remove("out.png")
-    else:
-        await event.client.send_file(event.chat_id, "out.png", force_document=False)
-        await event.delete()
-        os.remove(a)
-        os.remove("out.png")
+async def _(event):
+    await _ncode(event)
+
+
