23f450319ba8f983cd1b49dbe7c20d76d209bcdf
Sachin Jangir <support@ItzSjDude.in>
2020-12-13 09:47:43 +0530

    Update and rename remove.bg.py to removebg.py

--- plugins/removebg.py
+++ plugins/removebg.py
@@ -1,9 +1,8 @@
 """**Remove image background***\n
 {i}rmbg <reply to image>
-{i}rmbg <Image link >
-"""
-from . import _rmbg
+{i}rmbg <Image link >"""
 
+from . import _rmbg
 
 @ItzSjDude(outgoing=True, pattern="rmbg ?(.*)")
 async def _(event):
