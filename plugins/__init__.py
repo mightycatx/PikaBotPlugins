419563ff72eba611c8cdefefc92efc457db53fda
Sachin Jangir <support@ItzSjDude.in>
2020-12-13 08:00:09 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4748,3 +4748,99 @@ async def _ncode(event):
     await a_.delete()
     os.remove(a)
     os.remove("out.png")
+
+async def _rmbg(event):
+    HELP_STR = (
+        "`.rmbg` as reply to a media, or give a link as an argument to this command"
+    )
+    if event.fwd_from:
+        return
+    if Var.REM_BG_API_KEY is None:
+        await event.edit("You need API token from remove.bg to use this plugin.")
+        return False
+    input_str = event.pattern_match.group(1)
+    start = pikatime.now()
+    message_id = event.message.id
+    _tg = await get_pika_tg(event)
+    a = await pika_msg(event, "Processing image for background Removal, Please wait...", _tg)
+    if event.reply_to_msg_id:
+        message_id = event.reply_to_msg_id
+        reply_message = await event.get_reply_message()
+        # check if media message
+        try:
+            downloaded_file_name = await event.client.download_media(
+                reply_message, Config.TMP_DOWNLOAD_DIRECTORY
+            )
+        except Exception as e:
+            await pika_msg(a, (str(e))
+            return
+        else:
+            await pika_msg(a, "Sending Image to ReMove.BG, Please wait...")
+            output_file_name = ReTrieveFile(downloaded_file_name)
+            os.remove(downloaded_file_name)
+    elif input_str:
+        await pika_msg(a, "Sending Image to ReMove.BG, Please wait...")
+        output_file_name = ReTrieveURL(input_str)
+    else:
+        await pika_msg(a, HELP_STR)
+        return
+    contentType = output_file_name.headers.get("content-type")
+    if "image" in contentType:
+        with io.BytesIO(output_file_name.content) as remove_bg_image:
+            remove_bg_image.name = "@PikaBot.png"
+            await event.client.send_file(
+                event.chat_id,
+                remove_bg_image,
+                force_document=True,
+                supports_streaming=False,
+                allow_cache=False,
+                reply_to=message_id,
+            )
+        end = pikatime.now()
+        ms = (end - start).seconds
+        await pika_msg(a, 
+            "Removed dat annoying Backgroup in {} seconds, powered by Pikachu UserBot".format(
+                ms
+            )
+        )
+    else:
+        await pika_msg(a, 
+            "ReMove.BG API returned Errors. Please report to @ItzSjDudeSupport\n`{}".format(
+                output_file_name.content.decode("UTF-8")
+            )
+        )
+
+
+# this method will call the API, and return in the appropriate format
+# with the name provided.
+def ReTrieveFile(input_file_name):
+    headers = {
+        "X-API-Key": Var.REM_BG_API_KEY,
+    }
+    files = {
+        "image_file": (input_file_name, open(input_file_name, "rb")),
+    }
+    r = requests.post(
+        "https://api.remove.bg/v1.0/removebg",
+        headers=headers,
+        files=files,
+        allow_redirects=True,
+        stream=True,
+    )
+    return r
+
+
+def ReTrieveURL(input_url):
+    headers = {
+        "X-API-Key": Var.REM_BG_API_KEY,
+    }
+    data = {"image_url": input_url}
+    r = requests.post(
+        "https://api.remove.bg/v1.0/removebg",
+        headers=headers,
+        data=data,
+        allow_redirects=True,
+        stream=True,
+    )
+    return r
+
