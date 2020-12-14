b12eb3ae16c4287f7a80d5319b61b4928d203202
ItzSjDude <Support@itzsjdude.in>
2020-12-14 15:18:06 +0000

    PikaBot Fixes🚶

--- plugins/systools.py
+++ plugins/systools.py
@@ -2,32 +2,31 @@
 {i}set|get|del var <VarName>
 **Usage**: set|get|del Heroku Vars\n
 {i}usage
-**Usage**: Shows Dynos usage\n 
-{i}logs 
+**Usage**: Shows Dynos usage\n
+{i}logs
 **Usage**: Sends your botlogs in current chat\n
-{i}restart 
+{i}restart
 **Usage**: Restarts Pikabot"""
 
-import asyncio
-import math
-import os
-import heroku3
-import requests
 
-from . import _vars, _dyno, _logs, _restart
+from . import _dyno, _logs, _restart, _vars
+
 
 @ItzSjDude(outgoing=True, pattern=r"(set|get|del) var(?: |$)(.*)(?: |$)([\s\S]*)")
 async def _(var):
-  await _vars(var)
+    await _vars(var)
+
 
 @ItzSjDude(outgoing=True, pattern=r"usage(?: |$)")
 async def _(dyno):
-  await _dyno(dyno)
+    await _dyno(dyno)
+
 
 @ItzSjDude(outgoing=True, pattern="restart")
 async def _(rstrt):
-  await _restart(rstrt)
+    await _restart(rstrt)
+
 
 @ItzSjDude(outgoing=True, pattern=r"logs")
 async def _logs(dyno):
-  await _logs(dyno)
+    await _logs(dyno)
