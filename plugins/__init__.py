b4e0c9620fa63d9e43303444ed87922f948f5805
Sachin Jangir <support@ItzSjDude.in>
2020-12-17 16:11:41 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -4937,3 +4937,186 @@ def convert_from_bytes(size):
         size /= power
         n += 1
     return f"{round(size, 2)} {units[n]}"
+
+import heroku3
+import requests
+Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
+app = Heroku.app(Var.HEROKU_APP_NAME)
+
+
+heroku_api = "https://api.heroku.com"
+async def _vars(var):
+    _tg = await get_pika_tg(var)
+    exe = var.pattern_match.group(1)
+    heroku_var = app.config()
+    if exe == "get":
+       a= await pika_msg(var, "`Getting information...`", _tg)
+        await asyncio.sleep(1.5)
+        try:
+            variable = var.pattern_match.group(2).split()[0]
+            if variable in heroku_var:
+                return await pika_msg(a, 
+                    "**ConfigVars**:" f"\n\n`{variable} = {heroku_var[variable]}`\n"
+                )
+            else:
+                return await pika_msg(a, 
+                    "**ConfigVars**:" f"\n\n`Error:\n-> {variable} don't exists`"
+                )
+        except IndexError:
+            configs = prettyjson(heroku_var.to_dict(), indent=2)
+            with open("configs.json", "w") as fp:
+                fp.write(configs)
+            with open("configs.json", "r") as fp:
+                result = fp.read()
+                if len(result) >= 4096:
+                    await var.client.send_file(
+                        var.chat_id,
+                        "configs.json",
+                        reply_to=var.id,
+                        caption="`Output too large, sending it as a file`",
+                    )
+                else:
+                    await pika_msg(a, 
+                        "`[HEROKU]` ConfigVars:\n\n"
+                        "================================"
+                        f"\n```{result}```\n"
+                        "================================"
+                    )
+            os.remove("configs.json")
+            return
+    elif exe == "set":
+        a = await pika_msg(var, "`Setting information...`", _tg)
+        variable = var.pattern_match.group(2)
+        if not variable:
+            return await pika_msg(a, ">`.set var <ConfigVars-name> <value>`")
+        value = var.pattern_match.group(3)
+        if not value:
+            variable = variable.split()[0]
+            try:
+                value = var.pattern_match.group(2).split()[1]
+            except IndexError:
+                return await pika_msg(a, ">`.set var <ConfigVars-name> <value>`")
+        await asyncio.sleep(1.5)
+        if variable in heroku_var:
+            await pika_msg(a, 
+                f"**{variable}**  `successfully changed to`  ->  **{value}**"
+            )
+        else:
+            await pika_msg(a, 
+                f"**{variable}**  `successfully added with value`  ->  **{value}**"
+            )
+        heroku_var[variable] = value
+    elif exe == "del":
+        a=await pika_msg(var, "`Getting information to deleting variable...`", _tg)
+        try:
+            variable = var.pattern_match.group(2).split()[0]
+        except IndexError:
+            return await pika_msg(a, "`Please specify ConfigVars you want to delete`")
+        await asyncio.sleep(1.5)
+        if variable in heroku_var:
+            await pika_msg(a, f"**{variable}**  `successfully deleted`")
+            del heroku_var[variable]
+        else:
+            return await pika_msg(a, f"**{variable}**  `is not exists`")
+
+
+async def _dyno(dyno):
+    """
+    Get your account Dyno Usage
+    """
+    a=await pika_msg(dyno, "`Calculating your Dyno Usage`", _tg)
+    useragent = (
+        "Mozilla/5.0 (Linux; Android 10; SM-G975F) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/80.0.3987.149 Mobile Safari/537.36"
+    )
+    user_id = Heroku.account().id
+    headers = {
+        "User-Agent": useragent,
+        "Authorization": f"Bearer {Var.HEROKU_API_KEY}",
+        "Accept": "application/vnd.heroku+json; version=3.account-quotas",
+    }
+    path = "/accounts/" + user_id + "/actions/get-quota"
+    r = requests.get(heroku_api + path, headers=headers)
+    if r.status_code != 200:
+        return await pika_msg(a, 
+            "`Error: something bad happened`\n\n" f">.`{r.reason}`\n"
+        )
+    result = r.json()
+    quota = result["account_quota"]
+    quota_used = result["quota_used"]
+
+    """ - Used - """
+    remaining_quota = quota - quota_used
+    percentage = math.floor(remaining_quota / quota * 100)
+    minutes_remaining = remaining_quota / 60
+    hours = math.floor(minutes_remaining / 60)
+    minutes = math.floor(minutes_remaining % 60)
+
+    """ - Current - """
+    App = result["apps"]
+    try:
+        App[0]["quota_used"]
+    except IndexError:
+        AppQuotaUsed = 0
+        AppPercentage = 0
+    else:
+        AppQuotaUsed = App[0]["quota_used"] / 60
+        AppPercentage = math.floor(App[0]["quota_used"] * 100 / quota)
+    AppHours = math.floor(AppQuotaUsed / 60)
+    AppMinutes = math.floor(AppQuotaUsed % 60)
+
+    await asyncio.sleep(1.5)
+
+    return await pika_msg(a, 
+        "**Dyno Usage**:\n\n"
+        f" -> `Dyno usage for`  **{Var.HEROKU_APP_NAME}**:\n"
+        f"     •  `{AppHours}`**h**  `{AppMinutes}`**m**  "
+        f"**|**  [`{AppPercentage}`**%**]"
+        "\n"
+        " -> `Dyno hours quota remaining this month`:\n"
+        f"     •  `{hours}`**h**  `{minutes}`**m**  "
+        f"**|**  [`{percentage}`**%**]"
+    )
+
+def prettyjson(obj, indent=2, maxlinelength=80):
+    """Renders JSON content with indentation and line splits/concatenations to fit maxlinelength.
+    Only dicts, lists and basic types are supported"""
+
+    items, _ = getsubitems(
+        obj,
+        itemkey="",
+        islast=True,
+        maxlinelength=maxlinelength - indent,
+        indent=indent,
+    )
+    return indentitems(items, indent, level=0)
+
+
+async def _restart(rstrt):
+    _tg= await get_pika_tg(rstrt)
+    await pika_msg(rstrt, "**Boss I am restarting!, Please wait for a min after that do {x}ping or {x}help**".format(x=rx), _tg)      
+    app.restart()
+
+
+async def _logs(dyno):
+    _tg = await get_pika_tg(dyno)
+    try:
+        Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
+        app = Heroku.app(Var.HEROKU_APP_NAME)
+    except BaseException:
+        return await dyno.reply(
+            " Please make sure your Heroku API Key, Your App name are configured correctly in the heroku Please Join @ItzSjDudeSupport For Any Issue"
+        )
+    await pika_msg(dyno, "Getting Logs....", _tg)
+    with open("logs.txt", "w") as log:
+        log.write(app.get_log())
+    await dyno.client.send_file(
+        dyno.chat_id,
+        "logs.txt",
+        reply_to=dyno.id,
+        caption="logs of 100+ lines",
+    )
+    await pika_msg(a, "Sending in Progress.......")
+    await asyncio.sleep(1)
+    await a.delete()
