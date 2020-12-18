a00b3c67b30f702e39d3928ee8118a7188217da6
ItzSjDude <Support@itzsjdude.in>
2020-12-18 03:49:59 +0000

    PikaBot Fixes🚶

--- plugins/tools.py
+++ plugins/tools.py
@@ -4,17 +4,19 @@ Available Commands:
 {i}tm <as reply to a media>
 {i}tt <as reply to a large text>
 """
-from . import apk, _invite, _telegraph
+from . import _invite, _telegraph, apk
+
 
 @ItzSjDude(outgoing=True, pattern="app (.*)")
 async def _(e):
     await apk(e)
 
+
 @ItzSjDude(outgoing=True, pattern="invite ?(.*)")
 async def _(event):
     await _invite(event)
 
+
 @ItzSjDude(outgoing=True, pattern="t(m|t) ?(.*)")
 async def _(event):
     await _telegraph(event)
-
