20cd78dd024253ef7b6dbd94dc6db80b3058dc18
Sachin Jangir <support@ItzSjDude.in>
2020-12-20 07:32:23 +0530

    Update reveal.py

--- plugins/reveal.py
+++ plugins/reveal.py
@@ -3,20 +3,7 @@
 """
 # Made By @ItzSjDude. All rights reserved
 
-import os
-
-
+from . import _reveal 
 @ItzSjDude(pattern=r"reveal")
 async def _(event):
-    b = await event.client.download_media(await event.get_reply_message())
-    a = open(b, "r")
-    c = a.read()
-    a.close()
-    a = await event.edit("**Reading file...**")
-    if len(c) > 4095:
-        await a.edit("`The Total words in this file is more than telegram limits.`")
-    else:
-        await event.client.send_message(event.chat_id, f"```{c}```")
-        await a.delete()
-    os.remove(b)
-    await event.delete()
+    await _reveal(event)
