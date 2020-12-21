09628be1a89c3066272d9b61192deec363e051ee
ItzSjDude <Support@itzsjdude.in>
2020-12-21 04:20:27 +0000

    PikaBot Fixes🚶

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -1,22 +1,24 @@
-#© Itzsjdude
+# © Itzsjdude
 
 
-from . import _welcome, set_wlcm, get_welcm, del_welcm
+from . import _welcome, del_welcm, get_welcm, set_wlcm
 
 
 @bot.on(ChatAction)
 async def _(_pika):
     await _welcome(_pika)
 
+
 @ItzSjDude(outgoing=True, pattern=r"setwelcome(?: |$)(.*)")
 async def _(_pika):
     await set_wlcm(_pika)
-    
+
+
 @ItzSjDude(outgoing=True, pattern="getwelcome$")
 async def _(_pika):
     await get_welcm(_pika)
-   
+
+
 @ItzSjDude(outgoing=True, pattern="delwelcome$")
 async def _(_pika):
     await del_welcm(_pika)
-    
