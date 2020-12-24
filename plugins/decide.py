b1856eb0ba63180e5ef3b971144ea98f4c33d62c
ItzSjDude <Support@itzsjdude.in>
2020-12-24 07:59:58 +0000

    PikaBot Fixes🚶

--- plugins/decide.py
+++ plugins/decide.py
@@ -1,7 +1,9 @@
 """Quickly make a decision
 {i}decide"""
 
-from . import decide 
+from . import decide
+
+
 @ItzSjDude(outgoing=True, pattern="decide")
 async def _(event):
     await decide(event)
