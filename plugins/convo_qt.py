bb11c7658ee662f98eb71e539c6db7c2e36f2a8b
Sachin Jangir <support@ItzSjDude.in>
2021-01-04 06:43:22 +0530

    Update convo_qt.py

--- plugins/convo_qt.py
+++ plugins/convo_qt.py
@@ -3,8 +3,8 @@
 """
 
 # inspired by @Deonnn's being_logical.py, edited by : @mahshook_bot
-
+from . import _convoqt
 
 @ItzSjDude(outgoing=True, pattern=r"convoqt")
 async def _(event):
-    await convoqt(event)
+    await _convoqt(event)
