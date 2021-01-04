c69e004cf29ce184b2f750f1045f24e953ef0d50
ItzSjDude <Support@itzsjdude.in>
2021-01-04 01:14:35 +0000

    PikaBot Fixes🚶

--- plugins/convo_qt.py
+++ plugins/convo_qt.py
@@ -5,6 +5,7 @@
 # inspired by @Deonnn's being_logical.py, edited by : @mahshook_bot
 from . import _convoqt
 
+
 @ItzSjDude(outgoing=True, pattern=r"convoqt")
 async def _(event):
     await _convoqt(event)
