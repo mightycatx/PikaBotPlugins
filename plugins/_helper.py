f95f0021a1ecff7f87626cc5b635ba2785588380
Sachin Jangir <support@ItzSjDude.in>
2021-01-05 08:24:16 +0530

    Update _helper.py

--- plugins/_helper.py
+++ plugins/_helper.py
@@ -1,6 +1,6 @@
 from . import helper, pika_sudo
 
-@bot.on(pika_sudo(from_client=1, pattern=r"help ?(.*)))
+@bot.on(pika_sudo(from_client=1, pattern=r"help ?(.*)"))
 @ItzSjDude(outgoing=True, pattern=r"help ?(.*)")
 async def _(event):
     await helper(event)
