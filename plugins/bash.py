def43236a1324d3a8b27cdb53fd13729efabda1f
ItzSjDude <Support@itzsjdude.in>
2021-03-03 06:34:23 +0000

    PikaBot Fixes🚶

--- plugins/bash.py
+++ plugins/bash.py
@@ -1,27 +1,35 @@
 """For executing linux/Gnu Commands
 {i}bash <cmd> """
 
-from . import _bash, pika_sudo, bot, bot2, bot3, bot4
+from . import _bash, bot, bot2, bot3, bot4, pika_sudo
 
 if bot:
-  @bot.on(pika_sudo(from_client=1, pattern="bash ?(.*)"))
-  async def _(event):
-      await _bash(event)
+
+    @bot.on(pika_sudo(from_client=1, pattern="bash ?(.*)"))
+    async def _(event):
+        await _bash(event)
+
 
 if bot2:
-  @bot2.on(pika_sudo(from_client=2, pattern="bash ?(.*)"))
-  async def _(event):
-      await _bash(event)
+
+    @bot2.on(pika_sudo(from_client=2, pattern="bash ?(.*)"))
+    async def _(event):
+        await _bash(event)
+
 
 if bot3:
-  @bot3.on(pika_sudo(from_client=3, pattern="bash ?(.*)"))
-  async def _(event):
-      await _bash(event)
+
+    @bot3.on(pika_sudo(from_client=3, pattern="bash ?(.*)"))
+    async def _(event):
+        await _bash(event)
+
 
 if bot4:
-  @bot4.on(pika_sudo(from_client=4, pattern="bash ?(.*)"))
-  async def _(event):
-      await _bash(event)
+
+    @bot4.on(pika_sudo(from_client=4, pattern="bash ?(.*)"))
+    async def _(event):
+        await _bash(event)
+
 
 @ItzSjDude(pattern="bash ?(.*)")
 async def _(event):
