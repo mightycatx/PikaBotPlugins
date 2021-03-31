b6fdcf148d8603f928a87dd6cfe9c432a191766f
ItzSjDude <Support@itzsjdude.in>
2021-03-31 01:59:24 +0000

    PikaBot Fixes🚶

--- plugins/greetings.py
+++ plugins/greetings.py
@@ -1,29 +1,33 @@
 """Greetings plugin
-{i}gm 
+{i}gm
 Usage: Good Morning art
 
-{i}gn 
+{i}gn
 Usage: Good Night art
 
-{i}lul 
+{i}lul
 Usage: Lol Art
 
 {i}like
-Usage: Like art 
+Usage: Like art
 """
 from pikabot.sql_helper.global_variables import *
+
+
 @ItzSjDude(pattern="gn$")
 async def gn(event):
     await event.edit(GN)
-    
-    
+
+
 @ItzSjDude(pattern="gm$")
 async def gm(event):
     await event.edit(GM)
 
+
 @ItzSjDude(pattern="lul$")
 async def _(event):
     await event.edit(LOL)
+
 
 @ItzSjDude(pattern="like$")
 async def _(event):
