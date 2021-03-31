91743fa137b954653cf26cdbfbc14910677564ed
Sachin Jangir <support@ItzSjDude.in>
2021-03-31 08:45:40 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -1,5 +1,13 @@
-# © Itzsjdude
-
+"""
+{i}setwelcome <welcome message> or reply to msg
+**Usage**: Sets the message as a welcome note in the chat
+{i}getwelcome 
+**Usage**: Shows current welcome note in the chat\n
+{i}delwelcome 
+**Usage**: Deletes the welcome note from current chat\n\n 
+**Available formats for formatting welcome Note**:
+`{mention}, {title}, {count}, {first}, {last}, {fullname}, {userid}, {username}, {my_first}, {my_fullname}, {my_last}, {my_mention}, {my_username}`
+"""
 
 from . import ChatAction, _welcome, del_welcm, get_welcm, set_wlcm
 
