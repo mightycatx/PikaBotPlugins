deb9caf706a3c503d80c1688c7454ba3949439be
Sachin Jangir <support@ItzSjDude.in>
2021-12-26 18:03:40 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -5300,11 +5300,6 @@ async def _restart(rstrt):
     )
     app.restart()
 
-
-Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
-app = Heroku.app(Var.HEROKU_APP_NAME)
-
-
 async def _logs(dyno):
     _tg = await get_pika_tg(dyno)
     a = await pika_msg(dyno, "Getting Logs....", _tg)
