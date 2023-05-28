8051179bd89f5fb26d45e77d121e09c4d5e070c5
mightycatx <134790812+mightycatx@users.noreply.github.com>
2023-05-28 10:59:46 +0530

    Update bash.py

--- plugins/bash.py
+++ plugins/bash.py
@@ -3,33 +3,6 @@
 
 from . import _bash, bot, bot2, bot3, bot4
 
-if bot:
-
-    @bot.on(pika_sudo(from_client=1, pattern="bash ?(.*)"))
-    async def _(event):
-        await _bash(event)
-
-
-if bot2:
-
-    @bot2.on(pika_sudo(from_client=2, pattern="bash ?(.*)"))
-    async def _(event):
-        await _bash(event)
-
-
-if bot3:
-
-    @bot3.on(pika_sudo(from_client=3, pattern="bash ?(.*)"))
-    async def _(event):
-        await _bash(event)
-
-
-if bot4:
-
-    @bot4.on(pika_sudo(from_client=4, pattern="bash ?(.*)"))
-    async def _(event):
-        await _bash(event)
-
 
 @ItzSjDude(pattern="bash ?(.*)")
 async def _(event):
